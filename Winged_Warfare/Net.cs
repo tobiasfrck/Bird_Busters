358934a3aa470555bc15e98fb55c41195d1b2d03
tobiasf <30748375+tobiasfrck@users.noreply.github.com>
2023-06-17 19:58:41 +0200

    net rotation works

--- Winged_Warfare/Net.cs
+++ Winged_Warfare/Net.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Data;
 using System.Diagnostics;
+using System.Numerics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Vector3 = Microsoft.Xna.Framework.Vector3;
 
 namespace Winged_Warfare
 {
@@ -27,6 +29,7 @@ namespace Winged_Warfare
             this.target = -spawnTarget;
             this.target.Y += -0.09f;
             this.velocity = 0f;
+            RotateNet();
         }
 
 
@@ -37,35 +40,38 @@ namespace Winged_Warfare
             this.position.Y = this.position.Y + this.velocity;
             velocity -= gravity;
 
-            Vector3 direction = this.position - this.lastPosition;
-            Vector3.Normalize(direction);       
 
             _drawableObject.Position = this.position;
-            _drawableObject.Rotation = Rad2Deg(direction);
+            RotateNet();
             _drawableObject.Update();
       //      Debug.WriteLine(this.position.X + "-"+this.position.Y + "-" + this.position.Z);
         }
 
-        private static Vector3 Rad2Deg(Vector3 rad)
+        private void RotateNet()
         {
-            Vector3 deg = new Vector3();
-            deg.X = MathHelper.ToDegrees(rad.X);
-            deg.Y = MathHelper.ToDegrees(rad.Y);
-            deg.Z = MathHelper.ToDegrees(rad.Z);
-            return deg;
+            Matrix lookAtMatrix = Matrix.CreateLookAt(this.lastPosition, this.position, Vector3.Up);
+
+            Vector3 rotation = new()
+            {
+                X = (float)Math.Atan2(lookAtMatrix.M32, lookAtMatrix.M33),
+                Y = (float)Math.Atan2(-lookAtMatrix.M31,
+                    Math.Sqrt(lookAtMatrix.M32 * lookAtMatrix.M32 + lookAtMatrix.M33 * lookAtMatrix.M33)),
+                Z = (float)Math.Atan2(lookAtMatrix.M21, lookAtMatrix.M11)
+            };
+
+            _drawableObject.Rotation = Rad2Deg(rotation);
         }
 
-        public static Vector3 DirToRotation(Vector3 dir)
-        {
-            Vector3 rotation = new Vector3();
-            float angleX = MathF.Acos(Vector3.Dot(dir, Vector3.Up));
-            float angleY = MathF.Acos(Vector3.Dot(dir, Vector3.Up));
-            float angleZ = MathF.Acos(Vector3.Dot(dir, Vector3.Up));
 
-            rotation.X = MathHelper.ToDegrees(angleX);
-            rotation.Y = MathHelper.ToDegrees(angleY);
-            rotation.Z = MathHelper.ToDegrees(angleZ);
-            return rotation;
+        private static Vector3 Rad2Deg(Vector3 rad)
+        {
+            Vector3 deg = new()
+            {
+                X = MathHelper.ToDegrees(rad.X),
+                Y = MathHelper.ToDegrees(rad.Y),
+                Z = MathHelper.ToDegrees(rad.Z)
+            };
+            return deg;
         }
 
         public void Draw()
